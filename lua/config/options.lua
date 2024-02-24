8e59e9955123c342fd580689defe82760b4ddbf0
Kris Scott <kscott91@gmail.com>
2024-02-24 10:36:17 -0600

    Add gruvbox file-browser keymaps

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show filename above buffer
+-- vim.opt.winbar = "%=%m %f"

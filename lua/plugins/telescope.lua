7f4301b41ed7f0582b99590ae9173db861c4f400
Kris Scott <kscott91@gmail.com>
2024-02-24 12:28:59 -0600

    Add harpoon

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,15 @@
-local builtin = require("telescope.builtin")
-
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-    { "<leader>pf", builtin.find_files, desc = "Project: Find Files" },
-    { "<C-p>", builtin.git_files },
-    {
-      "<leader>ps",
-      function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end,
-      desc = "Project: Grep Search",
-    },
-    { "<leader>sx", builtin.resume, noremap = true, silent = true, desc = "Resume" },
+    --{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Project: Find Files" },
+    { "<C-p>", "<cmd>Telescope find_files<cr>" },
+    -- {
+    --   "<leader>ps",
+    --   function()
+    --     require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+    --   end,
+    --   desc = "Project: Grep Search",
+    -- },
+    { "<leader>sx", "<cmd>Telescope resume<cr>", noremap = true, silent = true, desc = "Resume" },
   },
 }

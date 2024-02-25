1b34e9f4a54c323f7f67117a785ceb673fea93d4
Kris Scott <kscott91@gmail.com>
2024-02-25 09:40:36 -0600

    Hide middle numbers in whichkey menu

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,6 +12,12 @@ local function goto_buffer_cmd(index)
   end
 end
 
+local whichkey = require("which-key")
 for i = 1, 9 do
   vim.keymap.set("n", "<leader>" .. i, goto_buffer_cmd(i), { desc = "Buffer " .. i })
+
+  -- Hide middle numbers to reduce which-key menu clutter
+  if i >= 2 and i <= 8 then
+    whichkey.register({ ["<leader>" .. i] = "which_key_ignore" })
+  end
 end

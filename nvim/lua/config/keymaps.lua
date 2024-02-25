0ce395f2c55d4662f1a00af3f2bb60fe7caa2d8c
Kris Scott <kscott91@gmail.com>
2024-02-24 20:46:54 -0600

    Replace harpoon with buffer selector keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--@param index integer
+local function goto_buffer_cmd(index)
+  return function()
+    vim.cmd("bf")
+    for _ = 2, index do
+      vim.cmd("bn")
+    end
+  end
+end
+
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, goto_buffer_cmd(i), { desc = "Buffer " .. i })
+end

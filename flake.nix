e75ef4900d2f611e2d690970faf6fc11e9a71af8
Kris Scott <kscott91@gmail.com>
2024-02-27 12:23:21 -0600

    Add nixos structure and default host

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "KrixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./nixos/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+}

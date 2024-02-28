abcf3b0ddd3c2f1e16ac9e85f6309e981634d475
Kris Scott <kscott91@gmail.com>
2024-02-28 08:17:12 -0600

    zsh: Fix keybinds for tmux

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,8 +1,13 @@
 bindkey -e
 
-# Fix Home/End/Delete/Ctrl-Backspace
-# This might be different on other terminals. Use `showkey -a` to check.
-bindkey  "^[[H"   beginning-of-line
-bindkey  "^[[F"   end-of-line
-bindkey  "^[[3~"  delete-char
-bindkey  "^H"     backward-kill-word
+# Key codes depend on the terminal. Use `showkey -a` to check.
+bindkey  "^[[H"   beginning-of-line   # Home
+bindkey  "^[[1~"  beginning-of-line   # Home (tmux)
+bindkey  "^[[F"   end-of-line         # End
+bindkey  "^[[4~"  end-of-line         # End (tmux)
+bindkey  "^[[3~"  delete-char         # Del
+bindkey  "^H"     backward-kill-word  # Ctrl+Backspace (no tmux equivalent--use Alt instead)
+bindkey "^[[1;3D" backward-word       # Alt+Left (tmux)
+bindkey "^[[1;3C" forward-word        # Alt+Right (tmux)
+bindkey "^[[1;5D" backward-word       # Ctrl+Left (tmux)
+bindkey "^[[1;5C" forward-word        # Ctrl+Right (tmux)

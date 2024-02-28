e6da5546930349da752dfee9add88e95367a2a2c
Kris Scott <kscott91@gmail.com>
2024-02-28 09:10:20 -0600

    tmux: add plugins to .nix

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -3,5 +3,20 @@
 {
   programs.tmux = {
     enable = true;
+    extraConfig = builtins.readFile ./tmux-init.conf;
+    # Note: 'sensible' plugin is included by default
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'mocha' # latte, frappe, macchiato, mocha
+          # Make tabs display window name instead of default (current directory)
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_text "#W"
+        '';
+      }
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.yank
+    ];
   };
 }

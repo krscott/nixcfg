c6c2d3851c042feca6e60d4f828de9943a48e354
Kris Scott <kscott91@gmail.com>
2024-02-28 09:20:15 -0600

    kitty: add nerdfont

--- home/personal.nix
+++ home/personal.nix
@@ -11,5 +11,6 @@
 
   home.packages = with pkgs; [
     firefox
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 }
--- kitty/kitty.nix
+++ kitty/kitty.nix
@@ -4,6 +4,8 @@
   programs.kitty = {
     enable = true;
     package = pkgs.kitty;
+    font.name = "JetBrainsMono Nerd Font";
+    font.size = 12;
     extraConfig = builtins.readFile ./kitty.conf;
   };
 }

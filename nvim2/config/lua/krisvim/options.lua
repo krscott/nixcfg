0ffb2db1efd7894a5660fe10f6d01d581f3ec6a5
Kris Scott <kscott91@gmail.com>
2024-03-01 14:30:45 -0600

    nvim: Add more opts

--- nvim2/config/lua/krisvim/options.lua
+++ nvim2/config/lua/krisvim/options.lua
@@ -1,11 +1,14 @@
-
 local opt = vim.opt
+
+local config_dir =
+    (os.getenv("XDG_CONFIG_HOME") or (os.getenv("HOME") .. "/.config"))
+    .. "/vim"
 
 opt.termguicolors = true
 
 -- Indents
 opt.autoindent = true
-opt.expandtab = true  -- Use spaces instead of tabs
+opt.expandtab = true -- Use spaces instead of tabs
 opt.shiftround = true
 opt.shiftwidth = 4
 opt.smartindent = true
@@ -16,8 +19,13 @@ opt.number = true
 opt.relativenumber = true
 
 -- Search
-opt.incsearch = true -- Find the next match as we type
-opt.hlsearch = true -- Highlight searches
+opt.incsearch = true  -- Find the next match as we type
+opt.hlsearch = true   -- Highlight searches
 opt.ignorecase = true -- Ignore case when searching...
-opt.smartcase = true -- ...unless we type a capital
+opt.smartcase = true  -- ...unless we type a capital
 
+-- Undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = config_dir .. "/undodir"
+vim.opt.undofile = true

0b552c331aca5197a06d317babb55c90f8415dff
Kris Scott <kscott91@gmail.com>
2024-03-01 14:20:49 -0600

    nvim: Enable installed LSPs

--- nvim2/config/after/plugin/lsp.lua
+++ nvim2/config/after/plugin/lsp.lua
@@ -1,10 +1,31 @@
 local lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local defaults = { capabilities = capabilities }
+
+lsp.bashls.setup(defaults)
+lsp.clangd.setup(defaults)
+lsp.cssls.setup(defaults)
+
+lsp.eslint.setup({
+  on_attach = function(_ --[[client]], bufnr)
+    -- Format document on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
+
+lsp.html.setup(defaults)
+lsp.jsonls.setup(defaults)
 
 lsp.lua_ls.setup({
   -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
           runtime = {
@@ -32,7 +53,7 @@ lsp.lua_ls.setup({
   end
 })
 
-lsp.tsserver.setup({})
-
-lsp.rust_analyzer.setup({})
-
+lsp.nil_ls.setup(defaults)
+lsp.pyright.setup(defaults)
+lsp.rust_analyzer.setup(defaults)
+lsp.tsserver.setup(defaults)

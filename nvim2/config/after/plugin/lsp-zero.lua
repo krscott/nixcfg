923563ea3136eba1ea0326259b00e4b0a8a0df6b
Kris Scott <kscott91@gmail.com>
2024-03-01 13:45:04 -0600

    nvim: Add completion mappings

--- nvim2/config/after/plugin/lsp-zero.lua
+++ nvim2/config/after/plugin/lsp-zero.lua
@@ -1,5 +1,6 @@
 local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
@@ -7,3 +8,25 @@ lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
+cmp.setup({
+    completion = {
+        -- Auto-select first item in completion menu
+        completeopt = 'menu,menuone,noinsert',
+    },
+
+    mapping = cmp.mapping.preset.insert({
+        -- `Tab` key to confirm completion
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    }),
+})

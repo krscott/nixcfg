a6594dda42f260303fe3d5ea2e4e80c98bd2e2a0
Kris Scott <kscott91@gmail.com>
2024-03-02 11:11:31 -0600

    Fix tmux/nvim colors

--- nvim2/config/after/plugin/catppuccin.lua
+++ nvim2/config/after/plugin/catppuccin.lua
@@ -1,3 +1,8 @@
 -- https://github.com/catppuccin/nvim
 
 vim.cmd.colorscheme "catppuccin"
+
+require("catppuccin").setup({
+    flavour = "mocha",             -- latte, frappe, macchiato, mocha
+    transparent_background = true, -- disables setting the background color.
+})

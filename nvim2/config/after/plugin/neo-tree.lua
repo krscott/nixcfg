4d21cdefb751efe34bd9f95c29fd9e2ba36ac88d
Kris Scott <kscott91@gmail.com>
2024-03-03 11:01:19 -0600

    nvim: Show dotfiles by default

--- nvim2/config/after/plugin/neo-tree.lua
+++ nvim2/config/after/plugin/neo-tree.lua
@@ -1,5 +1,18 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim/
 
+require('neo-tree').setup({
+    close_if_last_window = true,
+    filesystem = {
+        filtered_items = {
+            hide_dotfiles = false,
+            hide_by_name = {
+                ".git",
+            },
+            never_show = {
+                ".git",
+            },
+        },
+    },
+})
+
 vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle reveal<cr>', { desc = "Neotree" })
-
-

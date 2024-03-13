1162f75e80e1df68aa953c63e2e7ca40d431691d
Kris Scott <kscott91@gmail.com>
2024-03-13 22:54:54 +0000

    Add gnome-settings.sh

---
+++ scripts/styx/gnome-settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+# default: "appmenu:close"
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+
+# default: false
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button "true"

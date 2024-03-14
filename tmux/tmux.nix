8ccea478a97cf3ea43c5eace20a7f5740c5a8ec7
Kris Scott <kscott91@gmail.com>
2024-03-14 12:23:18 -0500

    Add tmux default shell

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -3,6 +3,7 @@
 {
   programs.tmux = {
     enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
     extraConfig = builtins.readFile ./tmux-init.conf;
     # Note: 'sensible' plugin is included by default
     plugins = with pkgs; [

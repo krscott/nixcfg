4d142c1eae031974e647c28d218c189710616309
Kris Scott <kscott91@gmail.com>
2024-03-14 10:49:13 -0500

    Add symlink to nerdfonts

--- font/nerdfont.nix
+++ font/nerdfont.nix
@@ -1,9 +1,11 @@
-{ config, pkgs, inputs, ... }:
+{ pkgs, ... }:
 let
   inherit (import ../options.nix) nerdfont;
+  nerdfontPkg = (pkgs.nerdfonts.override { fonts = [ nerdfont ]; });
 in
 {
-  home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ nerdfont ]; })
-  ];
+  home.packages = [ nerdfontPkg ];
+
+  # Required for running kitty with distrobox-export
+  home.file.".local/share/fonts/nixpkg-nerdfonts".source = "${nerdfontPkg}/share/fonts";
 }

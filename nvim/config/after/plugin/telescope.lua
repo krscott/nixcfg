decf35168395b38ec87f117544abf3c199b6ac35
Kris Scott <kscott91@gmail.com>
2024-03-14 12:04:27 -0500

    Improve find_files bind

--- nvim/config/after/plugin/telescope.lua
+++ nvim/config/after/plugin/telescope.lua
@@ -1,7 +1,13 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: Find Files in Git" })
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find Files" })
+
+function find_files()
+    builtin.find_files({
+        find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
+    })
+end
+
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: Find Files in Git" })
+vim.keymap.set('n', '<leader>pf', find_files, { desc = "Find Files" })
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "Buffers" })
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = "Help" })
-

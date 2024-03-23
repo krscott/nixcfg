87c9f8c4ff51e1dda805cc6a845f6c27ef4553dd
Kris Scott <kscott91@gmail.com>
2024-03-23 16:56:07 -0500

    Add xinput

--- hosts/styx/styx-home.nix
+++ hosts/styx/styx-home.nix
@@ -1,9 +1,13 @@
-{ specialArgs, ... }: 
+{ pkgs, specialArgs, ... }: 
 let
   inherit (specialArgs.inputs) nixGL;
 in
 {
   nixGLPrefix = "${nixGL.packages.x86_64-linux.nixGLNvidia}/bin/nixGLNvidia-550.54.14";
+
+  home.packages = with pkgs; [
+    xorg.xinput
+  ];
 
   systemd.user.services.fix-touch = {
     Unit.Description = "Fix touch display settings";

d31b46ef8a6c78d10593c6dde37ebdc6f6629626
Kris Scott <kscott91@gmail.com>
2024-03-23 16:56:22 -0500

    Change font

--- options.nix
+++ options.nix
@@ -1,15 +1,17 @@
-rec {
+{
   mainUsername = "kris";
   gitEmail = "kscott91@gmail.com";
   gitFullName = "Kris Scott";
 
 
-  nerdfont = "Iosevka";
-  # nerdfont = "JetBrainsMono";
-  fontName = "${nerdfont} Nerd Font";
+  # nerdfont = "Iosevka";
+  # fontName = "$Iosevka Nerd Font";
 
-  # nerdfont = "FantasqueSansMono";
-  # fontName = "FantasqueSansM Nerd Font";
+  # nerdfont = "JetBrainsMono";
+  # fontName = "$JetBrainsMono Nerd Font";
+
+  nerdfont = "FantasqueSansMono";
+  fontName = "FantasqueSansM Nerd Font";
 
   fontSize = 16;
 }

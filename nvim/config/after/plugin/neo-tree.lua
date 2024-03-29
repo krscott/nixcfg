268a5b8819d252c72d4accc3eb0b3f1da0c85b25
Kris Scott <kscott91@gmail.com>
2024-03-28 19:22:48 -0500

    nvim: Add neo-tree width, diag symbols

--- nvim/config/after/plugin/neo-tree.lua
+++ nvim/config/after/plugin/neo-tree.lua
@@ -13,6 +13,19 @@ require('neo-tree').setup({
             },
         },
     },
+    window = {
+        width = 30,
+    }
 })
 
-vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle reveal<cr>', { desc = "Neotree" })
+vim.fn.sign_define("DiagnosticSignError",
+    { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn",
+    { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo",
+    { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint",
+    { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle reveal<cr>',
+    { desc = "Neotree" })

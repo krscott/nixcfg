c397483bee4ec4e96c73e6e106e587d1303ad217
Kris Scott <kscott91@gmail.com>
2024-07-02 09:09:08 -0500

    Reorder startup scripts

--- hosts/styx/startup.sh
+++ hosts/styx/startup.sh
@@ -11,12 +11,11 @@ shopt -s failglob
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 isdef() {
-	type "$1" > /dev/null 2>&1
+	type "$1" >/dev/null 2>&1
 }
-
-distrobox enter ubuntu-nix -- bash -ic './fix-touch.sh'
 
 isdef rclone || ./rclone-install.sh
 # Configure gdrive manually: https://rclone.org/drive/
 isdef rclone && [[ -d "$HOME/gdrive" ]] && rclone mount gdrive: "$HOME/gdrive" --daemon
 
+distrobox enter ubuntu-nix -- bash -ic './fix-touch.sh'

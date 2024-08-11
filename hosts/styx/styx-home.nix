f419f1ae1b7a480a89e24e1f5fb76f40417db74c
Kris Scott <kscott91@gmail.com>
2024-08-11 13:24:10 -0500

    Add gnome-monitor-config aliases

--- hosts/styx/styx-home.nix
+++ hosts/styx/styx-home.nix
@@ -8,4 +8,10 @@ in
   home.packages = with pkgs; [
     xorg.xinput
   ];
+
+  home.shellAliases = {
+    mon219 = "gnome-monitor-config set -LpM DP-1 -t normal -m '3440x1440@144.001' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 1208 -y 1440";
+    mon169 = "gnome-monitor-config set -LpM DP-1 -t normal -m '2560x1440@143.999' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 768 -y 1440
+";
+  };
 }

d83a98b73fcd681460aa96b37a00919c951d0790
Kris Scott <kscott91@gmail.com>
2024-08-12 07:32:00 -0500

    Change mon alias to script bin

--- hosts/styx/styx-home.nix
+++ hosts/styx/styx-home.nix
@@ -1,17 +1,24 @@
-{ pkgs, specialArgs, ... }: 
+{ pkgs, inputs, specialArgs, ... }:
 let
   inherit (specialArgs.inputs) nixGL;
+
+  gmc = "${inputs.gnome-monitor-config.packages."${pkgs.system}".default}/bin/gnome-monitor-config";
 in
 {
   nixGLPrefix = "${nixGL.packages.x86_64-linux.nixGLNvidia}/bin/nixGLNvidia-*";
 
   home.packages = with pkgs; [
     xorg.xinput
-  ];
 
-  home.shellAliases = {
-    mon219 = "gnome-monitor-config set -LpM DP-1 -t normal -m '3440x1440@144.001' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 1208 -y 1440";
-    mon169 = "gnome-monitor-config set -LpM DP-1 -t normal -m '2560x1440@143.999' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 768 -y 1440
-";
-  };
+    (
+      writeShellScriptBin "mon169" ''
+        ${gmc} set -LpM DP-1 -t normal -m '2560x1440@143.999' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 768 -y 1440
+      ''
+    )
+    (
+      writeShellScriptBin "mon219" ''
+        ${gmc} set -LpM DP-1 -t normal -m '3440x1440@144.001' -LM HDMI-1 -t normal -m '1024x600@59.821' -x 1208 -y 1440
+      ''
+    )
+  ];
 }

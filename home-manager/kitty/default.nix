9114920016dfe2dfa1ea04e1f58823ad562cf3db
Kris Scott <kscott91@gmail.com>
2024-09-14 12:53:51 -0700

    Add kitty desktop item

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -15,18 +15,32 @@ in
     fontSize = krslib.mkIntOption "Font Size" 16;
   };
 
-  config = lib.mkIf config.krs.kitty.enable {
-    home.shellAliases = {
-      # Install kitty terminfo on servers
-      #s = "kitty +kitten ssh";
-    };
+  config = lib.mkIf config.krs.kitty.enable (
+    let
+      kittyPkg = (config.nixgl pkgs.kitty);
+      kittyDesktopItem = pkgs.makeDesktopItem {
+        name = "kitty";
+        desktopName = "kitty";
+        exec = "${kittyPkg}/bin/kitty";
+        icon = "${kittyPkg}/lib/kitty/logo/kitty-128.png";
+        categories = [ "Utility" "TerminalEmulator"];
+      };
+    in 
+    {
+      home.shellAliases = {
+        # Install kitty terminfo on servers
+        #s = "kitty +kitten ssh";
+      };
 
-    programs.kitty = {
-      enable = true;
-      package = (config.nixgl pkgs.kitty);
-      font.name = config.krs.kitty.fontName;
-      font.size = config.krs.kitty.fontSize;
-      extraConfig = builtins.readFile ./kitty.conf;
-    };
-  };
+      programs.kitty = {
+        enable = true;
+        package = kittyPkg;
+        font.name = config.krs.kitty.fontName;
+        font.size = config.krs.kitty.fontSize;
+        extraConfig = builtins.readFile ./kitty.conf;
+      };
+
+      home.file.".local/share/applications/kitty.desktop".text = builtins.readFile "${kittyDesktopItem}/share/applications/kitty.desktop";
+    }
+  );
 }

da23c9bbb804ec8866468e049a758ef3d6a175a0
Kris Scott <kscott91@gmail.com>
2024-09-16 02:08:26 -0700

    Improve git log aliases

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -16,7 +16,12 @@
       };
       aliases = {
         s = "status";
-        lol = "log --oneline --date-order";
+        ss = "status -sb";
+        lol = "log --graph --abbrev-commit --date-order --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+        lola = "lol --all";
+        lol2 = "log --graph --abbrev-commit --date-order --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lola2 = "lol2 --all";
+        dogs = "log --decorate --oneline --graph --stat";
         k = "!gitk --date-order";
         ka = "!gitk --all --date-order";
         l = "!lazygit";

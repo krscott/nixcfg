4407565656f0188fb84b7eeaa5df6e9a976f93f1
Kris Scott <kscott91@gmail.com>
2024-09-16 04:05:57 -0700

    Combine git status aliases

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -15,8 +15,7 @@
         };
       };
       aliases = {
-        s = "status";
-        ss = "status -sb";
+        s = "status -sb";
         lol = "log --graph --abbrev-commit --date-order --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
         lola = "lol --all";
         lol2 = "log --graph --abbrev-commit --date-order --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";

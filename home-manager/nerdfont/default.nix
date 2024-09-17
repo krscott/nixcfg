c70d0b3a0299a9284d3f782dd7e8699de22492bf
Kris Scott <kscott91@gmail.com>
2024-09-17 14:08:08 -0700

    Clean up nerdfonts nix

--- home-manager/nerdfont/default.nix
+++ home-manager/nerdfont/default.nix
@@ -7,21 +7,13 @@
     };
   };
 
-  config =
-    let
-      inherit (config.krs.nerdfonts) fonts;
+  config = {
+    home.packages = [
+      (pkgs.nerdfonts.override {
+        fonts = config.krs.nerdfonts.fonts;
+      })
+    ];
 
-      nerdfontPkg = (pkgs.nerdfonts.override { inherit fonts; });
-
-      # symlinkFont = fontFamily: {
-      #   ".local/share/fonts/${fontFamily}".source = "${nerdfontPkg}/share/fonts/truetype/NerdFonts";
-      # };
-    in
-    {
-      home.packages = [ nerdfontPkg ];
-
-      fonts.fontconfig.enable = true;
-
-      # home.file = lib.mkMerge (builtins.map symlinkFont fonts);
-    };
+    fonts.fontconfig.enable = true;
+  };
 }

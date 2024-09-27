54f481ca22ab799264ce4fd8dc8203fe06df68c9
Kris Scott <kscott91@gmail.com>
2024-09-27 08:43:16 -0700

    nvim: Add find all files hotkey

--- home-manager/nvim/config/after/plugin/telescope.lua
+++ home-manager/nvim/config/after/plugin/telescope.lua
@@ -2,12 +2,19 @@ local builtin = require('telescope.builtin')
 
 local function find_files()
     builtin.find_files({
-        find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
+        find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' }
+    })
+end
+
+local function find_all_files()
+    builtin.find_files({
+        find_command = { 'rg', '--files', '--hidden', '--no-ignore', '--glob', '!**/.git/*' }
     })
 end
 
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: Find Files in Git" })
 vim.keymap.set('n', '<leader>pf', find_files, { desc = "Find Files" })
+vim.keymap.set('n', '<leader>pF', find_all_files, { desc = "Find All Files" })
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "Buffers" })
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = "Help" })

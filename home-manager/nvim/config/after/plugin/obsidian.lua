2e68cdb142bc9452686d1d06a0f13921370869c5
Kris Scott <kscott91@gmail.com>
2024-10-02 16:02:41 -0700

    nvim: Fix obsidian error if no workspace

--- home-manager/nvim/config/after/plugin/obsidian.lua
+++ home-manager/nvim/config/after/plugin/obsidian.lua
@@ -1,14 +1,34 @@
-require("obsidian").setup({
-    workspaces = {
-        {
-            name = "notes",
-            path = "~/Documents/notes",
-        },
-    },
-    ui = {
-        checkboxes = {
-            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-            ["x"] = { char = "", hl_group = "ObsidianDone" },
-        },
+local function is_dir(path)
+    local stat = vim.loop.fs_stat(path)
+    return stat and stat.type == "directory"
+end
+
+local function filter_extant_dirs(workspaces)
+    local out = {}
+    for _, w in ipairs(workspaces) do
+        if is_dir(w.path) then
+            table.insert(out, w)
+        end
+    end
+    return out
+end
+
+local workspaces = filter_extant_dirs({
+    {
+        name = "notes",
+        path = "~/Documents/notes",
     },
 })
+
+-- plugin errors on startup if workspaces is empty
+if #workspaces > 0 then
+    require("obsidian").setup({
+        workspaces = workspaces,
+        ui = {
+            checkboxes = {
+                [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+                ["x"] = { char = "", hl_group = "ObsidianDone" },
+            },
+        },
+    })
+end
